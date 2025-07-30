cb3143ec90dfc6c8da504011266c1ea23d02061d
Awnish Kumar Sharma <awnishmmg.a41@gmail.com>
2025-07-31 01:19:57 +0530

    React Js

---
+++ React/react-apps/app1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//This is a App component: functional component
+function App() {
+  return (
+    <React.Fragment>
+      <h1> Welcome to React</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ React/react-apps/app1/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const ROOT = document.getElementById("root");
+const VROOT = ReactDOM.createRoot(ROOT);
+
+VROOT.render(<App />);
