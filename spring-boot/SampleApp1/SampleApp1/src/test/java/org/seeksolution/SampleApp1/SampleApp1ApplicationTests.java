6fd6cb18ea0892fad38e9bf4b0f8cf17d6f4e2e3
Awnish Kumar <awnishmmg.a41@gmail.com>
2025-08-12 22:05:13 +0530

    changes till date

---
+++ spring-boot/SampleApp1/SampleApp1/src/test/java/org/seeksolution/SampleApp1/SampleApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seeksolution.SampleApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
